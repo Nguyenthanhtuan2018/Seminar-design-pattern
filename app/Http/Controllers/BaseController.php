739ae2d372406556499b05fd9818fe408c70cb69
[Lampart] Nguyen Thanh Tuan <thanh_tuan@lampart-vn.com>
2019-08-07 15:51:28 +0700

    First commit

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+abstract class BaseController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+}
